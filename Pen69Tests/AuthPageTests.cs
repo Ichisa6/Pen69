5b105fb026a73e44f6465cf3d77630258f81f184
Ichisa6 <90610727+Ichisa6@users.noreply.github.com>
2023-04-17 09:26:22 +0300

    Update AuthPageTests.cs

--- Pen69Tests/AuthPageTests.cs
+++ Pen69Tests/AuthPageTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pen69.Pages;
 using Pen69Tests.Model;
 using System;
@@ -12,6 +12,7 @@ namespace Pen69.Pages.Tests
     [TestClass()]
     public class AuthPageTests
     {
+
         [TestMethod()]
         public void AuthPageTest()
         {
@@ -20,5 +21,52 @@ namespace Pen69.Pages.Tests
             var loginCheck = DB.User.Where(x => x.Login == login).FirstOrDefault();
             Assert.IsTrue(loginCheck != null);
         }
+
+        [TestMethod()]
+        public void AddPageTest()
+        {
+
+            Pencil1Entities db = new Pencil1Entities();
+            string Login = "765";
+            string Password = "2";
+            string Name = "3";
+
+            User user = new User { Login = Login, Name = Name, Password = Password };
+            db.User.Add(user);
+            db.SaveChanges();
+            var logincheck2 = db.User.Where(x => x.Login == Login).FirstOrDefault();
+
+            Assert.IsTrue(logincheck2 != null);
+        }
+
+        [TestMethod()]
+        public void AddEditPageTest()
+        {
+            Pencil1Entities db = new Pencil1Entities();
+            string login = "765";
+            string buffer = "asdad";
+
+            var logincheck = db.User.Where(x => x.Login == login).FirstOrDefault();
+            logincheck.Name = buffer;
+            db.SaveChanges();
+            var logincheck2 = db.User.Where(x => x.Login == login).FirstOrDefault();
+
+            Assert.IsTrue(logincheck2.Name == buffer);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+
+            Pencil1Entities db = new Pencil1Entities();
+            string login = "765";
+            var logincheck = db.User.Where(x => x.Login == login).FirstOrDefault();
+            db.User.Remove(logincheck);
+            db.SaveChanges();
+            var logincheck2 = db.User.Where(x => x.Login == login).FirstOrDefault();
+
+            Assert.IsTrue(logincheck2 == null);
+        }
     }
+    
 }
